0d56fb071628dc3f33c90d7f806371c076303551
sodaphish <coreyjsteele@gmail.com>
2017-10-16 18:40:11 -0500

    add autoGonk -- hopefully our self-determining "brain" to automate this

---
+++ autoGonk.py
@@ -0,0 +1,16 @@
+"""
+autoGonk -- testing out auto-determining which interfaces serve which function
+"""
+
+class ArpTable():
+    arpTable = {}
+    
+    pass
+
+class RouteTable():
+    routeTable = {}
+    
+    pass
+
+class IP():
+    pass
