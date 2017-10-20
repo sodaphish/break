6690241dbfa0d9322f4a7ce97b324257b33facf4
sodaphish <coreyjsteele@gmail.com>
2017-10-19 21:51:00 -0500

    working on automating autoGonk to configure everything dynamically.
    OUI DB added to aid in autoconfiguration

--- autoGonk.py
+++ autoGonk.py
@@ -1,92 +1,98 @@
 """
-autoGonk -- testing out auto-determining which interfaces serve which function
+autoGonk -- auto-configuration script for break
+
+Step 0: be vewy quiet, wew hunting wabbits! (silence on the wire)
+Step 1: calculate victim IP and MAC address 
+Step 2: calculate gateway IP and MAC
+Step 3: ?
+Step 4: write our configuration to file
 """
 
 import sys
 from os import getuid
-from threading import Thread
-from ipaddress import IPv4Address
 
 try:
-    import pcapy
     from scapy.all import *
-    from pcapy import findalldevs, open_live, open_offline
-    from impacket.ImpactDecoder import EthDecoder, LinuxSLLDecoder
 except ImportError as e:
     print("[E] %s" % e)
-    sys.exit(1)
+    sys.exit(2)
 
 
-from break_config import *
+class MACAddress():
+    oui = "00:00:00"
+    machine = "00:00:00"
+    
+    def __init__(self):
+        pass
 
+class Victim():
+    def __init__(self,ip='0.0.0.0',mac='00:00:00:00:00:00'):
+        pass
+    
+    def setIP(self,ip='0.0.0.0'):
+        #validate IP -- don't allow RFC 3927 169.254/16 
+        pass
+    
+    def setMAC(self,mac='00:00:00:00:00:00'):
+        #validate MAC address, confirm its in the OUI database
+        pass
+    
+    def __repr__(self):
+        pass
+    
+    
+    
+class Gateway():
+    
+    def __init__(self,ip='0.0.0.0',mac='00:00:00:00:00:00'):
+        pass
+    
+    def setIP(self,ip='0.0.0.0'):
+        pass
+    
+    def setMAC(self,mac='00:00:00:00:00:00'):
+        pass
+    
+    def __repr__(self):
+        pass
 
-class PacketProcessor(Thread):
-    '''
-    pre: takes an open pcap device and itterates packets until pre-conditions are met... 
-    post: will pre-populate the things we need to pull-off our MiTM regardless of how we're cabled
-    '''
-
-    def __init__(self, pcapObj):
-        datalink = pcapObj.datalink()
-        if pcapy.DLT_EN10MB == datalink:
-            self.decoder = EthDecoder()
-        elif pcapy.DLT_LINUX_SSL == datalink:
-            self.decoder = LinuxSSLDecoder()
-        else:
-            raise Exception("Datalink type not supported: " % datalink)
-
-        self.pcap = pcapObj
-        Thread.__init__(self)
-
-    def run(self):
-        self.pcap.loop(0, self.packetHandler)
-
-    def packetHandler(self, hdr, data):
-        frame = self.decoder.decode(data)
-
-
-def getInterfaces(datalink=1, fil="eth"):
-    '''
-    returns a list of devices of the specified datalink type
-    see the end of `pydoc pcapy` for a full list of datalink types based on 
-    the filter specified.
-    '''
-    retval = []
-    for interface in findalldevs():
-        try:
-            z = open_live(interface, 1500, 0, 100)
-            if z.datalink() == datalink:
-                if str(interface).startswith(fil):
-                    retval.append(interface)
-        except:
-            # let the bodies hit the floor!
-            pass
-    return retval
 
 
 if __name__ == "__main__":
-    '''
-    perform unit testing of our classes and functions
-    '''
-    '''
-    print("[I] capturing on %s" % iface)
-    iface = getInterfaces(fil="en")[0]
-    print("[I] capturing on %s" % iface)
+    # TODO: this will eventually use a live capture, which will require root...
+    
     if getuid() > 0:
-        print "[E] you must be root"
+        print "[E] must be root!"
         sys.exit(2)
-
-    print("[I] quitting...")
-    sys.exit()
-    '''
-
-    capfile = '/Users/sodaphish/Desktop/sample.pcapng'
+        
+    capfile = '/home/sodaphish/Desktop/sample.pcap'
     a = rdpcap(capfile)
     sessions = a.sessions()
 
     for session in sessions:
         for pkt in sessions[session]:
             try:
-                print pkt[ARP], pkt[ARP]
+                # pkt[ARP].psrc will have the IP of the target/victim and 
+                # pkt[ARP].hwsrc will have the target/victim's MAC address
+                #print pkt[ARP].hwsrc, pkt[ARP].hwdst, pkt[ARP].psrc, pkt[ARP].pdst
+                pass
+            except:
+                #only concerned with arp packets.
+                pass
+            
+            try:
+                # the TTL threshold will be important.  we'll also want to 
+                # combine it with knowlege gained about the target/victim.
+                if int(pkt[IP].ttl) < 38:
+                    print pkt[Ether].src, pkt[Ether].dst, int(pkt[IP].ttl)
             except:
                 pass
+            
+
+
+
+
+
+
+            
+            
